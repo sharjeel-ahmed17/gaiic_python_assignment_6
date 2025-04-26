005b09a29235f02e8fb85f5b75f84c7cb00cacbc
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-04-26 22:08:13 +0500

    third_question_completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #     name  = self.name
 #     marks = self.marks
 
-# assignment 1 
+# 1. Using self 
 class Student :
     def __init__(self , name , marks):
         self.name = name 
@@ -15,7 +15,30 @@ class Student :
 student1 = Student("usman" , 100)
 student1.display()
 
-# assignment 2
+# 2. Using cls
 
 class Counter:
-    pass
+    count = 0
+
+
+    def __init__(self):
+        Counter.count += 1
+
+    @classmethod
+    def display_obj(cls):
+        print(f"total no. of objects created {cls.count}")
+
+
+# 3. Public Variables and Methods
+
+
+class Car:
+    def __init__(self , brands):
+        self.brands = brands
+    
+    def start(self):
+        print(f"{self.brands} car has started")
+
+my_car = Car("Toyota")
+print(my_car.brands)
+my_car.start()
